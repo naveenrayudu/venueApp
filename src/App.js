5231dfe749e57e5b30c65ea17a05bfceb8eec855
Naveen Rayudu <naveenkumar.rayudu@gmail.com>
2019-01-05 10:25:41 -0800

    venueapp

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './resources/styles.css';
+import { Element } from 'react-scroll';
+
+import Header from './components/header_footer/Header';
+import Featured from './components/featured';
+import VenueInfo from './components/venueInfo';
+import HighLights from './components/Highlights';
+import Pricing from './components/pricing';
+import Location from './components/location';
+import Footer from './components/header_footer/Footer';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App" style={{ height: '1500px', backgroundColor: 'blue' }}>
+				<Header />
+
+				<Element name="Event Starts In">
+					<Featured />
+				</Element>
+				<Element name="Venue INFO">
+					<VenueInfo />
+				</Element>
+				<Element name="Highlights">
+					<HighLights />
+				</Element>
+				<Element name="Pricing">
+					<Pricing />
+				</Element>
+				<Element name="Location">
+					<Location />
+				</Element>
+
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
